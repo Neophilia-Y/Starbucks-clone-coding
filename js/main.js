8f8f93e0a214cee1283e06bd18c9ee837f0e5fb2
Justin <neophilia85@gmail.com>
2022-12-01 23:24:55 +0100

    global Badge container, using GSAP cdn for animation

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ searchInputEl.addEventListener("blur", () => {
     searchInputEl.setAttribute("placeholder", "");
     searchInputEl.value = "";
 })
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener('scroll', _.throttle(() => {
+    if (window.scrollY > 500) {
+        //badge hide
+        gsap.to(badgeEl, 0.6, {
+            opacity: 0,
+            display: "none",
+        })
+    }
+    else {
+        //badge show
+        gsap.to(badgeEl, 0.4, {
+            opacity: 1,
+            display: "block",
+        })
+    }
+}, 300));
+// _.throttle(handler, delay time)
