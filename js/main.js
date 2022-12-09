2a445fe9a0429623f51300a2f5afb3793f313df9
Justin <neophilia85@gmail.com>
2022-12-09 11:41:03 +0100

    Add vertical slide, horizontal slide and toggle button for Promotion

--- js/main.js
+++ js/main.js
@@ -36,7 +36,6 @@ window.addEventListener('scroll', _.throttle(() => {
 
 // Fade in
 const fadeEls = document.querySelectorAll(".visual .fade-in");
-console.log(fadeEls)
 fadeEls.forEach((el, index) => {
     //gsap.to(element, time(second), option)
     gsap.to(el, 1, {
@@ -44,3 +43,50 @@ fadeEls.forEach((el, index) => {
         opacity: 1,
     })
 });
+
+//Vertical Loop
+const verticalSwiper = document.querySelector(".swiper-container-vertical");
+const verticalSwiperEl = verticalSwiper.querySelectorAll(".swiper");
+const verticalLoop = () => {
+    verticalSwiperEl.forEach((el, index) => {
+        window.setTimeout(() => {
+            verticalSwiper.scrollTop = verticalSwiper.clientHeight * index;
+        }, 2000 * index)
+
+    })
+}
+verticalLoop();
+setInterval(verticalLoop, 2000 * verticalSwiperEl.length)
+
+//Horizontal Swiper
+const HorizontalSwiper = document.querySelector(".swiper-container-horizontal");
+HorizontalSwiper.scrollLeft = 400;
+const leftSwiper = document.querySelector(".notice .left-icon");
+const rightSwiper = document.querySelector(".notice .right-icon");
+leftSwiper.addEventListener("click", () => {
+    HorizontalSwiper.scrollLeft -= 840;
+    if (HorizontalSwiper.scrollLeft == 0) {
+        leftSwiper.style.display = "none";
+
+    }
+    if (rightSwiper.style.display = "none") {
+        rightSwiper.style.display = "block"
+    }
+
+})
+rightSwiper.addEventListener("click", () => {
+    HorizontalSwiper.scrollLeft += 840;
+    if (HorizontalSwiper.scrollLeft > HorizontalSwiper.scrollWidth - 820) {
+        rightSwiper.style.display = "none";
+    }
+    if (leftSwiper.style.display = "none") {
+        leftSwiper.style.display = "block"
+    }
+})
+
+//Promotion toggle button
+const promotionToggle = document.querySelector(".inner .inner__right .toggle-promotion");
+const promotionContainer = document.querySelector(".notice .swiper-wrapper");
+promotionToggle.addEventListener("click", () => {
+    promotionContainer.classList.toggle("hide")
+})
